59ae0d5cf702e2b6884daca74d414e97b04da3d4
Rindy Fisk <117949294+photocabana@users.noreply.github.com>
2023-06-02 14:28:04 -0500

    Add files via upload

---
+++ Fisk_Rindy_Likes_Vol_1/script.js
@@ -0,0 +1,8 @@
+var likeCount = 3;
+var volume1 = document.querySelector("#count1");
+
+function like() {
+    likeCount++;
+    volume1.innerText = likeCount + " like(s)";
+    addLikes = document.querySelector(id)
+}
